b719a0b031d81215b6616c7455b94525defb3181
Suryakant Mudke <suryakantmudke1234@gmail.com>
2023-10-19 00:55:24 +0530

    recursive dfs traversals added

---
+++ BinaryTreesStriver/BinaryTrees/src/BinaryTrees/BinaryTreeTraversals.java
@@ -0,0 +1,51 @@
+package BinaryTrees;
+
+public class BinaryTreeTraversals  {
+
+    public  static void preOrder(Node root){
+
+        if(root==null) {
+            return;
+        }
+        System.out.print(root.data+" ");
+        preOrder(root.left);
+        preOrder(root.right);
+
+
+    }
+
+    public  static void inOrder(Node root){
+
+        if(root==null) {
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data+" ");
+        inOrder(root.right);
+
+
+    }
+    public  static void postOrder(Node root){
+
+        if(root==null) {
+            return;
+        }
+
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data+" ");
+
+
+    }
+
+    public static  void main(String[] args){
+
+        Node root=BinaryTreeInput.takeinput(true,false,0);
+        preOrder(root);
+        System.out.println();
+        inOrder(root);
+        System.out.println();
+        postOrder(root);
+
+    }
+}
